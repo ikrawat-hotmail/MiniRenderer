5664f28f200debe908064c9ca83d732f7b00278d
ikrawat-hotmail <49373920+ikrawat-hotmail@users.noreply.github.com>
2026-01-18 17:03:33 +0530

    Create modelLoaderTest.cpp

---
+++ rigidBody/test/modelLoaderTest.cpp
@@ -0,0 +1,21 @@
+#include "modelLoader.h"
+#include "rigidBody2D.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::vector<std::shared_ptr<RigidBody2D>> somethingunused;
+    cout << somethingunused.size() << endl;
+    cout << "=======================================" << endl;
+    ModelLoader &modelLoaderInstance = ModelLoader::getInstance();
+    modelLoaderInstance.init(somethingunused);
+
+    cout << "=======================================" << endl;
+    cout << somethingunused.size() << endl;
+    for (auto& eachBody : somethingunused) {
+        std::cout << eachBody->mId << endl;
+    }
+    return 0;
+}
